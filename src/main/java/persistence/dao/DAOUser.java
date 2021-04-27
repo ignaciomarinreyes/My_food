9702cd7ff3f0200ce464bd0bea380a7b583cdaeb
ignaciomarinreyes <ignaciomarinreyes@gmail.com>
2021-04-27 14:18:16 +0100

    login daoUser

--- src/main/java/persistence/dao/DAOUser.java
+++ src/main/java/persistence/dao/DAOUser.java
@@ -1,9 +1,35 @@
 
 package persistence.dao;
 
+import java.util.List;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
 import model.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import persistence.HibernateUtil;
 
 
 public class DAOUser extends DAOBase<User>{
+    
+    @Transactional
+    public User login(String name, String password) {
+        List<User> usuarios = null;
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Query query = session.createNativeQuery("SELECT * FROM user WHERE nickName = ? AND password = ?", User.class);
+            query.setParameter(1, name);
+            query.setParameter(2, password);
+            usuarios = query.getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return usuarios.isEmpty() ? null : usuarios.get(0);
+    }
     
 }
