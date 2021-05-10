eff7591bfdd7af7a9627bc3b40f587a81a7a7525
Luis Galindo <luiko.gmolina@gmail.com>
2021-05-10 18:18:57 +0100

    RegisterUserCommand implemented

--- src/main/java/commands/RegisterUserCommand.java
+++ src/main/java/commands/RegisterUserCommand.java
@@ -1,8 +1,20 @@
 package commands;
 
+import model.User;
+import persistence.dao.DAOUser;
+
 public class RegisterUserCommand extends FrontCommand {
     @Override
     public void process() {
+        DAOUser daoUser = new DAOUser();
 
+        String nickname = request.getParameter("nickname");
+        String password = request.getParameter("firstPassword");
+        String fullname = request.getParameter("fullname");
+
+        User user = new User(nickname, password, fullname);
+        daoUser.create(user);
+
+        forward("/index.jsp");
     }
 }
